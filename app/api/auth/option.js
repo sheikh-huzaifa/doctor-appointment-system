dcfeec978c57fdbe7ad436eaed44ce03bc36b0ad
Huzaifa Hameed <123205960+sheikh-huzaifa@users.noreply.github.com>
2023-12-11 00:10:45 +0500

    dibuahgduio

--- app/api/auth/option.js
+++ app/api/auth/option.js
@@ -32,7 +32,20 @@ import CredentialsProvider from "next-auth/providers/credentials";
         })
     ],
     secret: "sdafasdfa345235rdasf",
-    
+    callbacks: {
+        async jwt({ token, account }) {
+          // Persist the OAuth access_token to the token right after signin
+          if (account) {
+            token.accessToken = account.access_token
+          }
+          return token
+        },
+        async session({ session, token, user }) {
+          // Send properties to the client, like an access_token from a provider.
+          session.accessToken = token.accessToken
+          return session
+        }
+      }
     
    
      
